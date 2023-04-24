adb361efd48ca3d6dcd4adbcb61a30b31b407300
Mahir-Neema <mohi10503@gmail.com>
2023-04-24 18:23:40 +0530

    added link to movie page at main page

--- src/app/components/MainPage.js
+++ src/app/components/MainPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import '../styles/mainpage.css'
+import Link from 'next/link'
 
 const MainPage = () => {
   return (
     <main className='main_section'>
         <div className='mainpage_container'>
+          <Link href="/movie" className='watch_movie_btn'>watch movie</Link>
+          
         </div>
     </main>
   )
