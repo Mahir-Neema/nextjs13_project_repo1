a4530d91b97962b3ff19966dbc49c480510876fd
Mahir-Neema <mohi10503@gmail.com>
2023-05-07 15:46:07 +0530

    added icon

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -9,7 +9,8 @@ const Header = () => {
     <header className="main_header">
         <div className="compl_header">
             <Link href="/">
-                <Image src="https://cdn.pixabay.com/photo/2022/08/22/22/19/sailor-7404656_1280.png" alt="image" width={70} height={70}/>
+                <Image src="https://cdn.pixabay.com/photo/2022/10/20/12/13/icons-7534723_960_720.png" alt="image" width={70} height={70}/>
+                {/* https://cdn.pixabay.com/photo/2013/07/12/14/05/shooting-star-147722_960_720.png */}
             </Link>
             <Nav/>
         </div>
