9fd1eaa76f6829641bb38937d5458997aeb7b660
Mahir-Neema <mohi10503@gmail.com>
2023-05-07 17:22:10 +0530

    logo not laoding removed

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -8,10 +8,9 @@ const Header = () => {
   return (
     <header className="main_header">
         <div className="compl_header">
-            <Link href="/">
-                <Image src="https://cdn.pixabay.com/photo/2022/10/20/12/13/icons-7534723_960_720.png" alt="image" width={70} height={70}/>
-                {/* https://cdn.pixabay.com/photo/2013/07/12/14/05/shooting-star-147722_960_720.png */}
-            </Link>
+            {/* <Link href="/">
+                <Image src="https://cdn.pixabay.com/photo/2022/10/20/12/13/icons-7534723_960_720.png" alt="image" width={30} height={30}/>
+            </Link> */}
             <Nav/>
         </div>
     </header>
--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-import styles from "../styles/navbar.css"
+import "../styles/navbar.css"
 
 const Nav = () => {
   return (
