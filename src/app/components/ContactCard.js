7df940567a94392c9e42564d155602ea76460757
Yash Seth <yashseth2002@gmail.com>
2023-05-10 20:04:11 +0530

    feat: change website structure to responsive

--- src/app/components/ContactCard.js
+++ src/app/components/ContactCard.js
@@ -59,19 +59,24 @@ const ContactCard = () => {
         <h2 className="contact-name">Contact Us</h2>
         
         <div className="contact-info">
-          <label for="fname" className="contact-label">Name</label>
-          <input className="contact-input" placeholder='Name' id="fname" type='text' name="username" value={user.username} onChange={handlechange}/>
-
-          <label for="email" className="contact-label">Email</label>
-          <input className="contact-input" placeholder='Email' id="email" type='email' name="email" value={user.email} onChange={handlechange}/>
-
-          <label for="phone" className="contact-label">Phone</label>
-          <input className="contact-input" placeholder='Phone' id="phone" type='phone' name="phone" value={user.phone} onChange={handlechange}/>
-
-          <label for="message" className="contact-label">Message</label>
-          <textarea className="contact-textarea" placeholder='Message' id="message" type='text' name="message" value={user.message} onChange={handlechange}/>
+          <div className="input-container">
+            <label for="fname" className="contact-label">Name</label>
+            <input className="contact-input" placeholder='Name' id="fname" type='text' name="username" value={user.username} onChange={handlechange}/>
+          </div>
+          <div className="input-container">
+            <label for="email" className="contact-label">Email</label>
+            <input className="contact-input" placeholder='Email' id="email" type='email' name="email" value={user.email} onChange={handlechange}/>
+          </div>
+          <div className='input-container'>
+            <label for="phone" className="contact-label">Phone</label>
+            <input className="contact-input" placeholder='Phone' id="phone" type='phone' name="phone" value={user.phone} onChange={handlechange}/>
+          </div>
+          <div className='input-container'>
+            <label for="message" className="contact-label">Message</label>
+            <textarea className="contact-textarea" placeholder='Message' id="message" type='text' name="message" value={user.message} onChange={handlechange}/>
+          </div>
+          <button className='submit_btn' onClick={handleSubmit} type='submit'>Submit</button>
         </div>
-        <button className='submit_btn' onClick={handleSubmit} type='submit'>Submit</button>
         {stats==="success" && <p style={{backgroundColor:"green"}} className='alert'>Thanks you for your message. We will contact you as soon as possible.</p>}
         {stats==="error" && <p style={{backgroundColor:"#f44336"}} className='alert'>Error Occured while submitting form.</p>}
       </form>
